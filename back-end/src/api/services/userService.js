1a46240f6b6e36eae3ab8f1577414f0efaf5c57d
Rafael Cavalcante <rafael.cavalcante.es@gmail.com>
2022-01-05 17:08:12 -0300

    codifica a senha com md5

--- back-end/src/api/services/userService.js
+++ back-end/src/api/services/userService.js
@@ -1,12 +1,14 @@
 const { User } = require('../../database/models');
 const { generateToken } = require('../middlewares/jwtToken');
-
+const crypto = require('crypto');
 
 const createCustomer = async ({name, email, password}) => {
   const role = 'customer'
-  await User.create({ name, email, password, role});
 
-  const token = generateToken(email, password, role);
+  const hashPassword = crypto.createHash('md5').update(password).digest('hex');
+  await User.create({ name, email, password: hashPassword, role});
+
+  const token = generateToken(email, hashPassword, role);
 
   return {
     name,
