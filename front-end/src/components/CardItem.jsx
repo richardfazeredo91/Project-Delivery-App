6d84020a2171c78262a3ce2eca9c3d67e4e309c3
Cesar Cançado Bhering <cesarcbhering@gmail.com>
2022-01-06 18:22:27 -0300

    Cria esqueleto do fluxo 3

---
+++ front-end/src/components/CardItem.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const CardItem = () => {
+  return (
+    <div>
+      <h1>Price</h1>
+      <img src="https://via.placeholder.com/150" alt="placeholder" />
+      <div>
+        <p>Title</p>
+        <div>
+          <button>-</button>
+          <p></p>
+          <button>+</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CardItem;
---
+++ front-end/src/pages/CustomerProducts/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CustomerProducts = () => {
+  return (
+    <div>
+      <h1>CustomerProducts</h1>
+    </div>
+  );
+}
+
+export default CustomerProducts;
