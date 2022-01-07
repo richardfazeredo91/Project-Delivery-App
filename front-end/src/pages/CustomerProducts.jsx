163990ea8841c0d7be0e1c9b4ace46e8f5de7199
Cesar Cançado Bhering <cesarcbhering@gmail.com>
2022-01-07 15:05:20 -0300

    alteraçoes linter

--- front-end/src/pages/CustomerProducts.jsx
+++ front-end/src/pages/CustomerProducts.jsx
@@ -2,15 +2,13 @@ import React from 'react';
 import Header from '../components/Header';
 import CardItem from '../components/CardItem';
 
-const CustomerProducts = () => {
-  return (
-    <div>
-      <h1>CustomerProducts</h1>
-      <Header />
-      {/* /product/listall */}
-      <CardItem />
-    </div>
-  );
-};
+const CustomerProducts = () => (
+  <div>
+    <h1>CustomerProducts</h1>
+    <Header />
+    {/* /product/listall */}
+    <CardItem />
+  </div>
+);
 
 export default CustomerProducts;
