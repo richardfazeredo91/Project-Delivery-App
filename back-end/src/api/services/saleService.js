8953e76aa900dd44a925bd8350f353aa858e583a
Michel Pereira <michelbecauro@gmail.com>
2022-01-07 15:43:27 -0300

    chore: Limpa alguns comentarios

--- back-end/src/api/services/saleService.js
+++ back-end/src/api/services/saleService.js
@@ -5,30 +5,14 @@ const getSales = async ( { user_id, role }) => {
   try {
     
     if (role === 'customer') {
-    
-      // Debuger
-      console.log('IF do SERVICE');
 
       const sales = await Sale.findAll({ where: { user_id }});
       
-      // Debuger
-      console.log('Retorno do banco:');
-      console.log(sales);
 
       if (!sales) throw Error('SALES_NOT_FOUND');
 
       return sales;
 
-      // return {
-      //   id: sales.id,
-      //   userId: sales.user_id,
-      //   sellerId: sales.seller_id,
-      //   totalPrice: sales.total_price,
-      //   deliveryAddress: sales.delivery_address,
-      //   deliveryNumber: sales.delivery_number,
-      //   saleDate: sales.sale_date,
-      //   status: sales.status,
-      // }
     }
 
     if (role === 'administrator') {
@@ -38,22 +22,10 @@ const getSales = async ( { user_id, role }) => {
       if (!sales) throw Error('SALES_NOT_FOUND');
   
         return sales
-      // return {
-      //   id: sales.id,
-      //   userId: sales.userId,
-      //   sellerId: sales.sellerId,
-      //   totalPrice: sales.totalPrice,
-      //   deliveryAddress: sales.deliveryAddress,
-      //   deliveryNumber: sales.deliveryNumber,
-      //   saleDate: sales.createdAt,
-      //   status: sales.status,
-      // }
     }
 
   } catch (error) {
-          // // Debuger
-          // console.log('Caiu no CAtch ERROr');
-          // console.log(error);
+
     return error;
   }
 }
