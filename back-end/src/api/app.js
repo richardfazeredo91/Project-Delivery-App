079fe0a0267ce3153608707c0c2c5489c14e9807
lima <joaopaulo.gomeslima8@gmail.com>
2022-01-11 17:06:44 -0300

    Adiciona ajuste para requisito 12.

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(cors());
 
 app.use(express.json());
-app.use('/images',express.static(path.normalize(`${__dirname}/../../public`)));
+app.use('/images', express.static(path.normalize(`${__dirname}/../../public`)));
 
 app.use('/user', userRoute);
 app.use('/sale', saleRoute);
